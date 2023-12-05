186a9368a7d447ede073417a84866d46170ce7f1
srinidhi-heydevops <150338045+srinidhi-heydevops@users.noreply.github.com>
2023-12-05 16:47:33 +0530

    Update StockManagerApplication.java
    Added health check

--- stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/StockManagerApplication.java
+++ stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/StockManagerApplication.java
@@ -8,5 +8,12 @@ public class StockManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StockManagerApplication.class, args);
+        
+livenessProbe:
+initialDelaySeconds: 2
+periodSeconds: 5
+httpGet:
+path: /health
+port: 9876
     }
 }
